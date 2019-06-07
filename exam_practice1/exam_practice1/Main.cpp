a2e74250d0098f047944f9771d785d99499124b1
khw0311 <khw000311@naver.com>
2019-06-07 21:23:30 +0900

    반복문_두께가 있는 별박스 만들기

--- exam_practice1/exam_practice1/Main.cpp
+++ exam_practice1/exam_practice1/Main.cpp
@@ -45,3 +45,71 @@ int main(void)
 
 	return 0;
 }
+
+
+
+	/*
+	while (true)
+	{
+		system("cls");
+
+		int iWidth, iHeight, iCount = 0;
+		int t = 0;	// 벽 두께
+
+		while (true)
+		{
+			switch (iCount)
+			{
+			case 0:
+				printf_s("너비를  입력하시오. ");
+				scanf_s("%d", &iWidth);
+
+				if (iWidth > 0 && iWidth < 21)
+					iCount++;
+				break;
+			case 1:
+				printf_s("높이를  입력하시오. ");
+				scanf_s("%d", &iHeight);
+
+				if (iHeight > 0 && iHeight < 21)
+					iCount++;
+				break;
+			case 2:
+				printf_s("두께를  입력하시오. ");
+				scanf_s("%d", &t);
+
+				if ((iWidth / 2) >= t && (iHeight / 2) >= t)
+					iCount++;
+				break;
+			}
+			if (iCount >= 3)
+				break;
+		}
+
+		//** 입력한 높이 값만큼 반복
+		for (int i = 0; i < iHeight; i++)
+		{
+			//** 입력한 폭 만큼 반복
+			for (int j = 0; j < iWidth; j++)
+			{
+				//** 만약
+				if (i < t ||	//** 현재 높이가 벽두께보다 작다면
+
+					//** (최대높이 - 두께) 값보다 현재높이가 클때
+					i >= iHeight - t ||
+
+					j < t || 	//** 현재 폭이 벽두께보다 작다면
+
+					//** (최대폭 - 두께) 값보다 현재폭이 클때
+					j >= iWidth - t)
+					//** 위 조건중 하나라도 만족한다면
+					printf_s("*");	//** 별을
+				else //** 아니면
+					printf_s(" "); //** 공백을 출력
+			}
+			printf_s("\n");
+		}
+		system("pause");
+		return 0;
+	}
+	*/
---
+++ exam_practice2/exam_practice2/Main.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+
+int main(void)
+{
+	int x, y, k, high;
+
+	printf("ют╥б : ");
+	scanf("%d", &high);
+
+	
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
